4940ab36ead3d3e1851e60d3cce699720e99f0dc
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-08-13 12:41:28 -0700

    lays out storyboards with custom classes.

---
+++ Journal/Journal/Cells/EntriesTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  EntriesTableViewCell.swift
+//  Journal
+//
+//  Created by Jonathan T. Miles on 8/13/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+import UIKit
+
+class EntriesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    
+}
---
+++ Journal/Journal/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Jonathan T. Miles on 8/13/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextField: UITextView!
+    
+}
