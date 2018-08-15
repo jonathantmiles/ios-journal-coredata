ba33dd1de2046de771047eec25421e4e4046f391
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-08-15 14:30:38 -0700

    adds Encodable enum with codingkeys

---
+++ Journal/Journal/Model/Entry+Encodable.swift
@@ -0,0 +1,28 @@
+//
+//  Entry+Encodable.swift
+//  Journal
+//
+//  Created by Jonathan T. Miles on 8/15/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+import Foundation
+
+extension Entry: Encodable {
+    enum CodingKeys: String, CodingKey {
+        case context
+        case title
+        case bodyText
+        case identifier
+        case timestamp
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode("context", forKey: .context)
+        try container.encode("title", forKey: .title)
+        try container.encode("bodyText", forKey: .bodyText)
+        try container.encode("identifier", forKey: .identifier)
+        try container.encode("timestamp", forKey: .timestamp)
+    }
+}
